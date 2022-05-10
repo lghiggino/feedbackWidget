f9f0e1cb187c9f23f199ec332783069ab732f052
lghiggino <lghiggino@gmail.com>
2022-05-10 11:06:25 -0300

    sets new team members from list

--- web/src/components/List.tsx
+++ web/src/components/List.tsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Button from "../components/Widget/Button"
+import { api } from "../lib/api"
 
 type ListProps = {
     initialItems: string[]
@@ -10,10 +11,15 @@ export default function List({ initialItems }: ListProps) {
     const [list, setList] = useState(initialItems)
     const [newItem, setNewItem] = useState('')
 
-    function addToList(text: string) {
-        setTimeout(() => {
-            setList(state => [...state, text])
-        }, 500)
+    async function addToList(text: string) {
+        setList(state => [...state, text])
+        try {
+            await api.post('/team', { name: text })
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setNewItem('')
+        }
     }
 
     function removeFromList(name: string) {
@@ -28,7 +34,7 @@ export default function List({ initialItems }: ListProps) {
                 placeholder="Novo usuário"
                 type="text"
                 value={newItem}
-                onChange={(ev) => { setNewItem(ev.target.value) }} 
+                onChange={(ev) => { setNewItem(ev.target.value) }}
                 className="min-w=[304px] min-h=[112px] mr-2 mb-4 text-xs placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin resize-none"
             />
 
@@ -41,7 +47,7 @@ export default function List({ initialItems }: ListProps) {
             <ul>
                 {list.map(item =>
                     <li key={item} className={"flex flex-row"} >
-                        <p className="mr-10 mb-1" style={{minWidth: '173px'}}>{item}</p>
+                        <p className="mr-10 mb-1" style={{ minWidth: '173px' }}>{item}</p>
                         <Button
                             onClick={() => { removeFromList(item) }}
                             text={"Remover"}
